8e430a0622348c3f95a3aed729d10dfc7843e8d3
Shockzz <projectshockzz@hotmail.co.uk>
2013-04-27 19:57:03 +0100

    Player now recieves the MOTD upon Joining.

--- src/com/github/PGMSOpenSource/OfficialPGMS/PGMS.java
+++ src/com/github/PGMSOpenSource/OfficialPGMS/PGMS.java
@@ -2,9 +2,12 @@ package com.github.PGMSOpenSource.OfficialPGMS;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.github.PGMSOpenSource.OfficialPGMS.events.JoinEvent;
 import com.github.PGMSOpenSource.OfficialPGMS.utils.PGMSLogger;
 
 public class PGMS extends JavaPlugin {
+	
+	public static PGMS plugin;
 	
 	public void onDisable() {
 		
@@ -12,6 +15,9 @@ public class PGMS extends JavaPlugin {
 	
 	public void onEnable() {
 		init();
+		getConfig().options().copyDefaults();
+		saveConfig();
+		getServer().getPluginManager().registerEvents(new JoinEvent(), this);
 	}
 	
 	public void init() {
--- src/com/github/PGMSOpenSource/OfficialPGMS/events/JoinEvent.java
+++ src/com/github/PGMSOpenSource/OfficialPGMS/events/JoinEvent.java
@@ -1,5 +1,19 @@
 package com.github.PGMSOpenSource.OfficialPGMS.events;
 
-public class JoinEvent {
-	//TODO: MOTD
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+
+import com.github.PGMSOpenSource.OfficialPGMS.PGMS;
+
+public class JoinEvent implements Listener {
+	
+	PGMS plugin;
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerLoginEvent e){
+		Player player = e.getPlayer();
+		player.sendMessage(plugin.getConfig().getString("motd"));
+	}
 }
