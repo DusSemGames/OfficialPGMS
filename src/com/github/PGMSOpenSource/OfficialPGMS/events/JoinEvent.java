b5929e4a691ab28d98b0edf0c7d506bff5a27b85
Shockzz <projectshockzz@hotmail.co.uk>
2013-04-28 12:17:55 +0100

    Fixed the MOTD.

--- src/com/github/PGMSOpenSource/OfficialPGMS/events/JoinEvent.java
+++ src/com/github/PGMSOpenSource/OfficialPGMS/events/JoinEvent.java
@@ -3,7 +3,7 @@ package com.github.PGMSOpenSource.OfficialPGMS.events;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 import com.github.PGMSOpenSource.OfficialPGMS.PGMS;
 
@@ -16,10 +16,10 @@ public class JoinEvent implements Listener {
 	}
 	
 	@EventHandler
-	public void onPlayerJoin(PlayerLoginEvent e){
+	public void onPlayerJoin(PlayerJoinEvent e){
 		Player player = e.getPlayer();
-		if(plugin.getConfig().contains("motd")) {
-			player.sendMessage(plugin.getConfig().getString("motd"));
-		}
+		String motd = plugin.getConfig().getString("motd");
+		motd = motd.replaceAll("&", "§");
+		player.sendMessage(motd);
 	}
 }
