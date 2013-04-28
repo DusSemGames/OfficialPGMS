53a7612a3c7610ffd26337ca2033b768d9eb6201
GuruFraser <frazz86@googlemail.com>
2013-04-28 12:57:54 +0100

    Team Utilities

---
+++ src/com/github/PGMSOpenSource/OfficialPGMS/utils/TeamUtils.java
@@ -0,0 +1,32 @@
+package com.github.PGMSOpenSource.OfficialPGMS.utils;
+
+import java.util.ArrayList;
+import com.github.PGMSOpenSource.OfficialPGMS.PGMS;
+
+public class TeamUtils 
+{
+	private PGMS plugin;
+	public TeamUtils(PGMS instance)
+	{
+		plugin = instance;
+	}
+	//TODO: Allow > 2 teams, handle teams via Start/End/Load Game
+	private static ArrayList<String> team1 = new ArrayList<String>();
+	private static ArrayList<String> team2 = new ArrayList<String>();
+	
+	public static boolean isObserver(String s)
+	{
+		return !team1.contains(s) || !team2.contains(s);
+	}
+	
+	public static boolean isTeam1(String s)
+	{
+		return team1.contains(s);
+	}
+	
+	public static boolean isTeam2(String s)
+	{
+		return team2.contains(s);
+	}
+	
+}
