d8425607fe4a8f2d3bb467c7986c102844159bd0
GuruFraser <frazz86@googlemail.com>
2013-04-28 12:27:50 +0100

    New Listeners

---
+++ src/com/github/PGMSOpenSource/OfficialPGMS/listeners/PlayerListener.java
@@ -0,0 +1,42 @@
+package com.github.PGMSOpenSource.OfficialPGMS.listeners;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import com.github.PGMSOpenSource.OfficialPGMS.PGMS;
+
+public class PlayerListener implements Listener
+{
+	private PGMS plugin;
+	
+	public PlayerListener(PGMS instance)
+	{
+		plugin = instance;
+	}
+	
+	@EventHandler
+	public void PlayerJoin(PlayerLoginEvent e)
+	{
+		Player p = e.getPlayer();
+		if(plugin.getConfig().contains("motd"))
+		{
+			p.sendMessage(plugin.getConfig().getString("motd"));
+		}
+	}
+	
+	@EventHandler
+	public void PlayerLogin(PlayerLoginEvent e)
+	{
+		//TODO: Ban checking
+	}
+	
+	public void PlayerQuit(PlayerQuitEvent e)
+	{
+		//TODO:
+		//Remove player from any lists etc.
+	}
+	
+}
